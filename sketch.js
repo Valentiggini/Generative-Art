69e32605368fddde451735269bddde73efe6c524
Valentiggini <71349260+Valentiggini@users.noreply.github.com>
2020-10-11 19:49:08 +0200

    updated sketch

--- sketch.js
+++ sketch.js
@@ -13,21 +13,18 @@ function setup() {
 }
 
 function draw() {
-
-
+  noFill();
   background('#24003D')
   translate(width / 2, height / 2);
 
 
-
-  noFill();
-  stroke(lerpColor(color('blue'), color('red'), frameCount / 60));
   rotate(frameCount * 6);
   strokeWeight(1);
 
+  const herpColorGradient = (cos(frameCount / 30) + 1) / 2;
 
   beginShape();
-
+  stroke(lerpColor(color('blue'), color('red'), herpColorGradient));
   for (var a = 0; a < TWO_PI * 8; a += 0.02) {
     var r = calculateRadius() * cos(k * a);
     var x = r * cos(a);
@@ -39,7 +36,10 @@ function draw() {
 
   //rosa2
 
+
   beginShape();
+
+  stroke(lerpColor(color('blue'), color('red'), herpColorGradient));
 
   for (var a = 0; a < TWO_PI * 2; a += 0.02) {
     var r = calculateSecondRadius() * cos(j * a);
@@ -52,10 +52,10 @@ function draw() {
 }
 
 function calculateRadius() {
-  return sin(frameCount * 0.02) * windowWidth;
+  return sin(frameCount * 0.02) * windowWidth / 2;
 }
 
 function calculateSecondRadius() {
-  return cos(frameCount * 0.02) * windowWidth;
+  return cos(frameCount * 0.02) * windowWidth / 2;
 }
 
