1915eaec0dfa65ef8d5bc410b8b90edd7b825ed5
Valentiggini <71349260+Valentiggini@users.noreply.github.com>
2020-10-11 20:57:42 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function preload() {
-
 }
+
 //The explosive Rose
 
-var k = 5 / 8;
-var j = 5 / 2;
+var k = 4 / 5;
+var j = 4 / 3;
 
 
 function setup() {
@@ -17,15 +17,16 @@ function draw() {
   background('#24003D')
   translate(width / 2, height / 2);
 
+  //forma1
 
-  rotate(frameCount * 6);
+  rotate(frameCount * 0.4);
   strokeWeight(1);
 
   const herpColorGradient = (cos(frameCount / 30) + 1) / 2;
 
   beginShape();
   stroke(lerpColor(color('blue'), color('red'), herpColorGradient));
-  for (var a = 0; a < TWO_PI * 8; a += 0.02) {
+  for (var a = 0; a < TWO_PI * 5; a += 0.02) {
     var r = calculateRadius() * cos(k * a);
     var x = r * cos(a);
     var y = r * sin(a);
@@ -34,28 +35,30 @@ function draw() {
   }
   endShape(CLOSE);
 
-  //rosa2
+  //forma2
 
+  strokeWeight(1.5);
 
   beginShape();
 
   stroke(lerpColor(color('blue'), color('red'), herpColorGradient));
 
-  for (var a = 0; a < TWO_PI * 2; a += 0.02) {
+  for (var a = 0; a < TWO_PI * 3; a += 0.02) {
     var r = calculateSecondRadius() * cos(j * a);
     var x = r * cos(a);
     var y = r * sin(a);
 
     vertex(x, y);
   }
+
   endShape(CLOSE);
 }
 
 function calculateRadius() {
-  return sin(frameCount * 0.02) * windowWidth / 2;
+  return sin(frameCount * 0.02) * windowWidth / 1.8;
 }
 
 function calculateSecondRadius() {
-  return cos(frameCount * 0.02) * windowWidth / 2;
-}
+  return cos(frameCount * 0.02) * windowWidth / 1.8;
 
+}
